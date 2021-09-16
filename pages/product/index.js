c1f9631a2e58138547fe529a8087982c15593145
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-16 18:31:12 +0500

    catch all routes

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 
-const product = () => {
+const productList = ({productId}) => {
     return (
-        <div className={styles.grid}>
-           <h3 className={styles.card}> My products</h3>
+        <>
+        <h3> My products</h3>
+
+        <div className={styles.grid , styles.card}>
+           <Link href='/product/watch'>
+               <a>Watch</a>
+           </Link>
         </div>
+        <div className={styles.grid , styles.card}>
+           <Link href='/product/dress'>
+               <a>
+                   Dress Faction
+               </a>
+           </Link>
+        </div>
+        <div className={styles.grid , styles.card}>
+           <Link href='/product/Shoes'>
+               <a>Shoes    </a>
+           </Link>
+        </div>
+        <div className={styles.grid , styles.card}>
+           <Link href={`/product/${productId}`} replace>
+               <a> product {productId}  </a>
+           </Link>
+        </div>
+        <Link href='/'>
+               <a>Back To Home</a>
+           </Link>
+        </>
     )
 }
 
-export default product
+export default productList
