db4ae24dc6be29795a1b8804365700a80b003bb8
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-16 18:48:11 +0500

    custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageNotFond = () => {
+    return (
+        <div>
+            <h3>404 | Page Style And Costumize</h3>
+        </div>
+    )
+}
+
+export default PageNotFond
