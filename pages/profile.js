1d02dbd562104655529b0cbffc5ed626a710c7ba
Nauman-Arshad <naumankhan642@gmail.com>
2021-09-16 20:28:29 +0500

    add tailwind

--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import styles from '../styles/Home.module.css'
 
 const profile = () => {
     return (
-        <div  className={styles.grid}>
-            <h3 className={styles.card}>Profile Check This Out</h3>    
-        </div>
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+             <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                    <image className="h-48 w-full object-cover md:h-full md:w-48" src="public/books.jpg" alt="Man looking at item at a store" />
+                    </div>
+    <div className="p-8">
+      <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+      <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p className="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div>
+
     )
 }
 
